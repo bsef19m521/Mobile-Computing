a1cc8e918a73d1dd8a73cf080c1ddc624fe13918
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-05-23 06:42:01 +0500

    This application is used to open Github Repository URL

--- RepositoryURL/app/src/main/java/com/example/myrepurl/MainActivity.java
+++ RepositoryURL/app/src/main/java/com/example/myrepurl/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.myrepurl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText t1;
+    private Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        URLListenOnButton();
+    }
+
+    public void URLListenOnButton() {
+        t1 = findViewById(R.id.editText1);
+        b1 = findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = t1.getText().toString();
+                Uri webpage = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
     }
 }
